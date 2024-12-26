db8643d118aa7bb60407891dce06dee7699f9c8f
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-26 18:44:16 +0530

    adding user schema

---
+++ backend/models/userSchema.js
@@ -0,0 +1,20 @@
+import mongoose  from "mongoose"
+
+mongoose.connect=process.env.MONGO_URL
+
+const userSchema = new mongoose.Schema({
+name:{
+    type:String,
+    require:true
+},
+email:{
+    type:String,
+    require:true
+},
+password:{
+    type:String,
+    require:true
+}
+})
+
+export const User = mongoose.model("User",userSchema)
