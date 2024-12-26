e8eec233dcef3831bbeacc3a156bc94fb31abb5e
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-26 10:21:38 +0530

    fix background

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -7,19 +7,24 @@ const Body = () => {
     {
       path: "/",
       element: <Login />,
-    },{
+    },
+    {
       path: "/browser",
       element: <Browesr />,
-    },{
+    },
+    {
       path: "/signup",
       element: <Signup />,
     },
   ]);
-  return <div>
-<RouterProvider router={appRouter}/>
+  return (
+    <div>
+      <div>
 
-
-
-  </div>;
+      {/* <img src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_large.jpg"/> */}
+      </div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
 };
 export default Body;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-
+import { IoSearch } from "react-icons/io5";
+import { TbLogout } from "react-icons/tb";
+import { IoIosArrowDropdown } from "react-icons/io";
 const Header = () => {
   return (
-    <div className="flex justify-between items-center bg-gradient-to-b from-black mt-2">
-      <img className="w-[190px]  "
+    <div className="  absolute   flex justify-between items-center bg-gradient-to-b from-black  w-[100%] p-8 px-12 ">
+    
+
+      <img
+        className=" pl-12 h-10"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png"
         alt="netflix-logo"
-      />
+        />
 
-      <div className="flex gap-4 mr-5">
-        <div>Rishabh fav</div>
-        <div>Logout</div>
-        <div>Serach Movie</div>
+      <div className="flex items-center ">
+        
+        <h1 className="text-lg font-medium text-white flex gap-1"><IoIosArrowDropdown size="26px " />Rishabh fav</h1>
+        <div className=" ml-4">
+          <button className="bg-red-800 text-white px-4 py-2   ">Logout</button>
+
+          <button className="bg-red-800 text-white px-4 py-2 ml-2 ">
+            SearchMovie
+          </button>
+        
       </div>
+        </div>
     </div>
   );
 };
