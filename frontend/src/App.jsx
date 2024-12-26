4b405c3cc9a758810897d72d140ee0eb65970228
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-26 09:21:31 +0530

    made app.jsx to work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Body from './components/Body'
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
-   <div>
-    <Body/>
-   </div>
-  )
+    <div>
+     
+      <Body />
+    </div>
+  );
 }
 
-export default App
+export default App;
