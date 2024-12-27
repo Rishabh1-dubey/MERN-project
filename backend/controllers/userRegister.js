ac299ca334bb3952822b932d0977c7d790662868
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-27 12:40:33 +0530

    done with user auth

--- backend/controllers/userRegister.js
+++ backend/controllers/userRegister.js
@@ -1,4 +1,5 @@
 import { User } from "../models/userSchema.js";
+import bcryptjs from "bcryptjs";
 
 const userRegister = async (req, res) => {
   try {
@@ -17,19 +18,19 @@ const userRegister = async (req, res) => {
         success: false,
       });
     }
+    const hasedPassword = await bcryptjs.hash(password, 12);
 
     await User.create({
       name,
       email,
-      password,
-    })
-return res.status(200).json({
-    message:"Account created Successfully"
-})
-
-
+      password: hasedPassword,
+      //because we gave a schema inthe name of password so we can not directly give the hashed value
+    });
+    return res.status(200).json({
+      message: "Account created Successfully",
+    });
   } catch (error) {
     console.log(error);
   }
 };
-export default userRegister
+export default userRegister;
