21041f634d95253cc09847e250ea231b37a1f899
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-27 12:41:30 +0530

    making initial changes

---
+++ backend/utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+// import dotenv from "dotenv"
+
+
+
+const uri ="mongodb+srv://renio067778:rishabh06@cluster0.n9bky.mongodb.net/"
+
+// dotenv.config({
+//     path:"../.env"
+// })
+const mongooseConnection =()=> {
+    console.log('MONGO_URL:', uri);
+
+    mongoose.connect(uri,{ useNewUrlParser: true ,useUnifiedTopology:true}).then(()=>{
+        console.log("mongoose data connected successfully")
+    }).catch((error)=>{
+        console.log("mongoose in not connected successfully",error);
+        
+    })
+}
+
+export default mongooseConnection
