7b5f9db8098192c6544ee1e498324953eacfa77d
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-28 21:53:24 +0530

    adding movie constent file

--- frontend/src/utils/constents.js
+++ frontend/src/utils/constents.js
@@ -1,1 +1,11 @@
 export const BACKEND_URL= "http://localhost:8080/api/v1/user"
+
+export const  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDVhNjliNWUxYjllYjcyNDEyNDk5ZjZlMzFlY2MxMiIsIm5iZiI6MTcxMDQzMDA3Ny4xMTYsInN1YiI6IjY1ZjMxNzdkYzQ5MDQ4MDE2MzE5ZWFiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UFDcZ-6zGaTnPQW3uupc-XQUhUaM4-nDHSGpPWdNVX4'
+    }
+  };
+
+export const MOVIE_URL ="https://api.themoviedb.org/3/movie"
