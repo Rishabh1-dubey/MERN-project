c5bbb8b13a6737b2e21b65ab78634219546b0fd1
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-29 10:45:27 +0530

    adding movie slice

--- frontend/src/redux/movieSlice.js
+++ frontend/src/redux/movieSlice.js
@@ -1,17 +1,32 @@
-
 import { createSlice } from "@reduxjs/toolkit";
-const movieSlice =createSlice({
-    name:"movie",
-    initialState:{
-        NowPlayingMovie:null
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    NowPlayingMovie: null,
+    NowPopularMovie: null,
+    NowUpcominngMovie: null,
+    NowTopRatedMovie: null
+  },
+  reducers: {
+    //action
+    now_Playing_Movie: (state, action) => {
+      state.NowPlayingMovie = action.payload;
     },
-    reducers:{
-        //action
-
-        now_Playing_Movie:(state,action)=>{
-            state.NowPlayingMovie= action.payload
-        }
-    }
-})
-export const{now_Playing_Movie}= movieSlice.actions
+    now_Popular_Movie: (state, action) => {
+      state.NowPopularMovie = action.payload;
+    },
+    now_Upcoming_Movie: (state, action) => {
+      state.NowUpcominngMovie = action.payload;
+    },
+    now_Top_Rated_Movie: (state, action) => {
+      state.NowTopRatedMovie = action.payload;
+    },
+  },
+});
+export const {
+  now_Playing_Movie,
+  now_Popular_Movie,
+  now_Upcoming_Movie,
+  now_Top_Rated_Movie,
+} = movieSlice.actions;
 export default movieSlice.reducer;
