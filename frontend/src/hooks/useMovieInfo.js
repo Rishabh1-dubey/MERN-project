077f1626b8b242567ff923a7dcc123db92ea7c75
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-30 18:19:04 +0530

    addding movieinfo

---
+++ frontend/src/hooks/useMovieInfo.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import {  options } from '../utils/constents'
+import { useDispatch } from 'react-redux'
+import { Set_Movie_Info } from '../redux/movieSlice'
+
+const useMovieInfo = (id) => {
+
+    const dispatch= useDispatch()
+ const Movieinfo= async()=>{
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/${id}`,options )
+    console.log(res.data)
+    dispatch(Set_Movie_Info(res.data))
+ }
+
+
+ useEffect(()=>{
+    Movieinfo()
+ },[])
+
+}
+
+
+export default useMovieInfo
