eaec1b251218851b04d007b31115db0c71635544
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-02 13:58:57 +0530

    adding movie trailer in redux slice

--- frontend/src/hooks/useMovieTrailer.js
+++ frontend/src/hooks/useMovieTrailer.js
@@ -1,21 +1,21 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { MOVIE_URL, options } from "../utils/constents";
-import { useDispatch } from "react-redux";
-import { Set_Movie__card_Trailer } from "../redux/movieSlice";
+// import axios from "axios";
+// import { useEffect } from "react";
+// import { MOVIE_URL, options } from "../utils/constents";
+// import { useDispatch } from "react-redux";
+// import { Set_Movie__card_Trailer } from "../redux/movieSlice";
 
-const useMovieTrailer = (id) => {
-  const dispatch = useDispatch();
+// const useMovieTrailer = (id) => {
+//   const dispatch = useDispatch();
   
-  const MovieTrailer = async () => {
-    const res = await axios.get(`${MOVIE_URL}/${id}/videos`, options);
-    console.log(res.data.results);
-    dispatch(Set_Movie__card_Trailer());
-  };
+//   const MovieTrailer = async () => {
+//     const res = await axios.get(`${MOVIE_URL}/${id}/videos`, options);
+//     console.log(res.data);
 
-  useEffect(() => {
-    MovieTrailer();
-  }, []);
-};
+//   };
 
-export default useMovieTrailer;
+//   useEffect(() => {
+//     MovieTrailer();
+//   }, []);
+// };
+
+// export default useMovieTrailer;
--- frontend/src/hooks/usePlayTrailer.js
+++ frontend/src/hooks/usePlayTrailer.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { options } from "../utils/constents";
 import { useDispatch } from "react-redux";
-import { Set_Trailer } from "../redux/movieSlice";
+import { Set_Movie__card_Trailer, Set_Trailer } from "../redux/movieSlice";
 import { useEffect } from "react";
 
 
-const UseTrailerMain=(movieId)=>{
+const UseTrailerMain=(movieId, flag)=>{
 
   const dispatch = useDispatch();
 
@@ -21,6 +21,8 @@ const UseTrailerMain=(movieId)=>{
         return  item.type === "Trailer";
       });
       dispatch(Set_Trailer(trailer.length  > 0 ? trailer[0] : trailer[1]));
+    dispatch(Set_Movie__card_Trailer(trailer));
+      console.log(trailer)
       
      
     }
