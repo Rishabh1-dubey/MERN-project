31b3aadf4e18a5116875a12684bcd69ac9d9e243
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-03 00:41:29 +0530

    adding cast data

--- frontend/src/components/CastandCrew.jsx
+++ frontend/src/components/CastandCrew.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import useMovieCredit from '../hooks/useMovieCredit'
-import { TMDB_IMG } from '../utils/constents'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import useMovieCredit from "../hooks/useMovieCredit";
+import { TMDB_IMG } from "../utils/constents";
 
 const CastandCrew = () => {
-    const {id} = useParams();
-    console.log(id)
-    useMovieCredit(id)
+  const { id } = useParams();
+  console.log(id);
+  useMovieCredit(id);
 
-    const movieCast = useSelector((state)=>state.movie.cast)
-    console.log(movieCast)
+  const movieCast = useSelector((state) => state.movie.cast);
+  console.log(movieCast);
   return (
-    <div className='w-[700px]'>
-         {/* <img
-        className=" w-44 h-full rounded-xl  cursor-pointer transform hover:scale-90 transition-all ease-out delay-100"
-        src={`https://image.tmdb.org/t/p/original/${movieCast?.profile_path}`}
-        alt="image-demo-ttesting"
-      /> */}
-      <h1 className='text-white'>{movieCast?.original_name}</h1>
-    </div>
-  )
-}
+    <div className="w-screen bg-black ">
+      <h1 className="text-white text-4xl">Cast and Crew</h1>
 
-export default CastandCrew
+      <div className="flex flex-wrap gap-3 overflow-x-hidden ">
+        {movieCast?.map((castmoive) => (
+          <div className="  w-44 h-full rounded-xl  cursor-pointer transform hover:scale-90 transition-all ease-out delay -100 px-4 py-4 ">
+            <img
+              className="rounded-lg h-56"
+              src={`${TMDB_IMG}/${castmoive?.profile_path}`}
+              alt="cast-image"
+            />
+            <h1 className="text-white">
+              {castmoive?.name || castmoive?.original_name}
+            </h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CastandCrew;
